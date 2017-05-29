b1b09bfcb6bffd419081a46a913ab86e269137d4
Gocchi <centenoilse93@gmail.com>
2017-05-29 15:38:17 -0500

    agregre jquery a las imagenes

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$("#btn-1").click(function(){
+    $("#img-1").css('display','block');
+    $("#img-2").hide();
+    $("#img-3").hide();
+    $("#img-4").hide();
+    
+});
+
+$("#btn-2").click(function(){
+    $("#img-1").hide();
+    $("#img-2").css('display','block');
+    $("#img-3").hide();
+    $("#img-4").hide();
+    
+});
+
+$("#btn-3").click(function(){
+    $("#img-1").hide();
+    $("#img-2").hide();
+    $("#img-3").css('display','block');
+    $("#img-4").hide();
+    
+});
+
+
+$("#btn-4").click(function(){
+    $("#img-1").hide();
+    $("#img-2").hide();
+    $("#img-3").hide();
+    $("#img-4").css('display','block');
+    
+});
+
+
+
+
+
+
+
+    
