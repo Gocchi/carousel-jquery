200dcbacba966358d7b6544bf348dc12128dc735
Gocchi <centenoilse93@gmail.com>
2017-05-29 16:13:32 -0500

    agrege jquery a cada boton

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $("#btn-1").click(function(){
     $("#img-2").hide();
     $("#img-3").hide();
     $("#img-4").hide();
+    $("#img-5").hide();
     
 });
 
@@ -11,6 +12,7 @@ $("#btn-2").click(function(){
     $("#img-2").css('display','block');
     $("#img-3").hide();
     $("#img-4").hide();
+    $("#img-5").hide();
     
 });
 
@@ -19,6 +21,7 @@ $("#btn-3").click(function(){
     $("#img-2").hide();
     $("#img-3").css('display','block');
     $("#img-4").hide();
+    $("#img-5").hide();
     
 });
 
@@ -28,7 +31,16 @@ $("#btn-4").click(function(){
     $("#img-2").hide();
     $("#img-3").hide();
     $("#img-4").css('display','block');
+    $("#img-5").hide();
     
+});
+
+$("#btn-5").click(function(){
+    $("#img-1").hide();
+    $("#img-2").hide();
+    $("#img-3").hide();
+    $("#img-4").hide();
+    $("#img-5").css('display','block');
 });
 
 
